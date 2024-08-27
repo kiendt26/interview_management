d677eafb529895f62dcec9941a27dcec34a1a7c4
Trung Kien <pttrungkien03@gmail.com>
2024-08-27 07:51:04 +0700

    first commit

--- src/main/java/fa/training/Application.java
+++ src/main/java/fa/training/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InterviewManagementApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(InterviewManagementApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
