87024d759a9e45ecee62c0830e3c3d2063d6caf0
nhat <nhatnmhe186348@fpt.edu.vn>
2024-09-01 23:35:39 +0700

    Update logic for OfferService

--- src/main/java/fa/training/services/OfferService.java
+++ src/main/java/fa/training/services/OfferService.java
@@ -1,9 +1,11 @@
 package fa.training.services;
 
+import fa.training.dto.OfferAddDTO;
 import fa.training.dto.OfferDTO;
 import fa.training.entities.Candidate;
 import fa.training.entities.Offer;
 import fa.training.entities.User;
+import fa.training.enums.Status;
 import fa.training.repositories.CandidateRepository;
 import fa.training.repositories.OfferRepository;
 import fa.training.repositories.UserRepository;
@@ -11,6 +13,7 @@ import fa.training.utils.DataHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.Objects;
 
 @Service
@@ -27,15 +30,60 @@ public class OfferService {
     @Autowired
     private UserRepository userRepository;
 
+    public boolean add(OfferAddDTO add) {
+        Candidate candidate = candidateRepository.findById(add.getCandidateId()).orElse(null);
 
-    //TODO: Validate From < To and Due
+        if (candidate == null) {
+            return false;
+        }
+
+        User approver = userRepository.findById(add.getApprover()).orElse(null);
+
+        if (approver == null) return false;
+
+        User recruiterOwner = userRepository.findById(add.getRecruiterOwnerId()).orElse(null);
+
+        if (recruiterOwner == null) return false;
+
+        if (add.getContractFrom().isAfter(add.getContractTo())) return false;
+
+        if (add.getBasicSalary().doubleValue() < 0) return false;
+
+        Offer offer = Offer.builder()
+                .candidate(candidate)
+                .approvalBy(approver)
+                .recruiterOwner(recruiterOwner.getUserName())
+                .position(add.getPosition())
+                .department(add.getDepartment())
+                .level(add.getLevel())
+                .contractFrom(add.getContractFrom())
+                .contractTo(add.getContractTo())
+                .dueDate(add.getDueDate())
+                .interviewNote(add.getInterviewNote())
+                .basicSalary(add.getBasicSalary())
+                .note(add.getNote())
+                .offerDate(LocalDate.now())
+                .status(Status.WAITING)
+                .build();
+
+        offerRepository.save(offer);
+
+        return true;
+    }
+
     public boolean update(OfferDTO update) {
+
+        if (update.getContractFrom().isAfter(update.getContractTo()))
+            return false;
 
         Offer offer = offerRepository.findById(update.getOfferId()).orElse(null);
 
-        if (offer == null) return false;
+        if (offer == null ||
+                offer.getStatus().equals(Status.REJECTED) ||
+                offer.getStatus().equals(Status.APPROVED))
+            return false;
 
-        User recruiteOwner =  userRepository.findByUserName(update.getRecruiterOwner());
+        User recruiteOwner = userRepository.findByUserName(update.getRecruiterOwner());
 
         Candidate candidate = null;
         if (!Objects.equals(offer.getCandidate().getCandidateId(), update.getCandidateId())) {
@@ -45,18 +93,20 @@ public class OfferService {
         String formattedNote = helper.format(update.getNote());
 
         // Set fields          |Call to DataHelper  |Old Data                   |New Data
-        offer.setCandidate(     helper.updateHelper( offer.getCandidate(),       candidate));
-        offer.setPosition(      helper.updateHelper( offer.getPosition(),        update.getPosition()));
-        offer.setInterviewInfo( helper.updateHelper( offer.getInterviewInfo(),   update.getInterviewInfo()));
-        offer.setContractFrom(  helper.updateHelper( offer.getContractFrom(),    update.getContractFrom()));
-        offer.setContractTo(    helper.updateHelper( offer.getContractTo(),      update.getContractTo()));
-        offer.setContractType(  helper.updateHelper( offer.getContractType(),    update.getContractType()));
-        offer.setLevel(         helper.updateHelper( offer.getLevel(),           update.getLevel()));
-        offer.setDepartment(    helper.updateHelper( offer.getDepartment(),      update.getDepartment()));
-        offer.setRecruiterOwner(helper.updateHelper( offer.getRecruiterOwner(),  recruiteOwner.getUserName()));
-        offer.setDueDate(       helper.updateHelper( offer.getDueDate(),         update.getDueDate()));
-        offer.setBasicSalary(   helper.updateHelper( offer.getBasicSalary(),     update.getBasicSalary()));
-        offer.setNote(          helper.updateHelper( offer.getNote(),            formattedNote));
+        offer.setCandidate(helper.updateHelper(offer.getCandidate(), candidate));
+        offer.setPosition(helper.updateHelper(offer.getPosition(), update.getPosition()));
+        offer.setInterviewInfo(helper.updateHelper(offer.getInterviewInfo(), update.getInterviewInfo()));
+        offer.setContractFrom(helper.updateHelper(offer.getContractFrom(), update.getContractFrom()));
+        offer.setContractTo(helper.updateHelper(offer.getContractTo(), update.getContractTo()));
+        offer.setContractType(helper.updateHelper(offer.getContractType(), update.getContractType()));
+        offer.setLevel(helper.updateHelper(offer.getLevel(), update.getLevel()));
+        offer.setDepartment(helper.updateHelper(offer.getDepartment(), update.getDepartment()));
+        offer.setRecruiterOwner(helper.updateHelper(offer.getRecruiterOwner(), recruiteOwner.getUserName()));
+        offer.setDueDate(helper.updateHelper(offer.getDueDate(), update.getDueDate()));
+        offer.setBasicSalary(helper.updateHelper(offer.getBasicSalary(), update.getBasicSalary()));
+        offer.setNote(helper.updateHelper(offer.getNote(), formattedNote));
+
+        offer.getCandidate().setStatus(Status.WAITING);
 
         offerRepository.save(offer);
 
