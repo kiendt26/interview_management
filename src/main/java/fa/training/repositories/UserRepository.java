f159f73f835d0ba731ab5c2214c6b2ca7ca73e8a
nhat <nhatnmhe186348@fpt.edu.vn>
2024-09-01 23:36:01 +0700

    Create roll as ENUM for User

--- src/main/java/fa/training/repositories/UserRepository.java
+++ src/main/java/fa/training/repositories/UserRepository.java
@@ -1,8 +1,12 @@
 package fa.training.repositories;
 
 import fa.training.entities.User;
+import fa.training.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUserName(String username);
+    List<User> findByRoleOrRole(Role role1, Role role2);
 }
