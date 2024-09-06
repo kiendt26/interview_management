7c95720c4d2cd8d07da3815152ffbcccf2b0ce7a
anhpvph22231 <107199089+phanv8776@users.noreply.github.com>
2024-09-06 11:48:27 +0700

    merge

--- src/main/java/fa/training/controllers/CandidateController.java
+++ src/main/java/fa/training/controllers/CandidateController.java
@@ -1,28 +1,8 @@
 package fa.training.controllers;
 
-import fa.training.entities.Candidate;
-import fa.training.repositories.CandidateRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
 
 @Controller
 public class CandidateController {
-    @Autowired
-    private CandidateRepository candidateRepository;
 
-    @GetMapping("/candidate-list")
-    public String listCandidates(Model model) {
-        List<Candidate> candidates = candidateRepository.findAll();
-        model.addAttribute("candidates", candidates);
-        return "candidate/candidate-list";
-    }
-    @GetMapping("/candidate-create")
-    public String createCandidateForm(Model model) {
-        model.addAttribute("candidate", new Candidate());
-        return "candidate/candidate-create";
-    }
 }
--- src/main/java/fa/training/repositories/CandidateRepository.java
+++ src/main/java/fa/training/repositories/CandidateRepository.java
@@ -1,6 +1,5 @@
 package fa.training.repositories;
 
-import fa.training.entities.Candidate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CandidateRepository extends JpaRepository<Candidate, Long> {
