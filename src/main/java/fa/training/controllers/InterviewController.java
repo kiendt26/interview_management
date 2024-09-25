bbf2984278c097ac4c66ed0afdb3d89a91f80017
Trung Kien <pttrungkien03@gmail.com>
2024-09-26 04:02:55 +0700

    add crud user

--- src/main/java/fa/training/controllers/InterviewController.java
+++ src/main/java/fa/training/controllers/InterviewController.java
@@ -16,11 +16,9 @@ import fa.training.services.InterviewServce;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
--- src/main/java/fa/training/entities/User.java
+++ src/main/java/fa/training/entities/User.java
@@ -2,6 +2,9 @@ package fa.training.entities;
 
 import fa.training.enums.Role;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Entity
@@ -16,11 +19,18 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
+    @NotBlank(message = "Username is required")
     private String userName;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String passwordHash;
     @Enumerated(EnumType.STRING)
+
     private Role role;
+
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is required")
     private String email;
-    private String passApp;
 
 }
